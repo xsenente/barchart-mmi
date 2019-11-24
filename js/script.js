7516405f6dd1577f8a62f464d53b66adb6eb608f
Xavier SENENTE <xavier.senente@gmail.com>
2019-11-24 18:36:54 +0100

    Etape 1 js

--- js/script.js
+++ js/script.js
@@ -1,230 +1,66 @@
-// Width and height
+// Largeur, hauteur et marges
 const margin = { top: 30, right: 20, bottom: 10, left: 400 },
-      width  = 800,
-      height = 600,
-      heightModule = height / 20,
-      duration = 1000;
+    width  = 800,
+    height = 600,
+    moduleHeight = 20;
 
-/** -----------------------------------
-  * Création du SVG
-  * ----------------------------------- */
+/**
+  * Créer un élément SVG
+  * la méthode select de D3 sélectionne un élément du DOM
+  * D3 utilise une technique appelée syntaxe en chaîne, que vous avec peut-être déjà vu en jQuery. En “enchaînant” les méthodes avec des points, vous pouvez effectuer plusieurs actions en une seule ligne de code.
+*/
+const svg = d3.select(".barchart")
 
-const svg = d3.select( ".barchart" )
-  .append( "svg" )
-    .attr( "viewBox", `0 0 ${ width } ${ height }` )
-  .append( "g" )
-    .attr( "transform", `translate( ${ margin.left }, ${ margin.top } )` );
+  // https://github.com/d3/d3-selection/blob/v1.4.0/README.md#selection_append
+  .append("svg")
 
-/** -----------------------------------
-  * Création de l'échelle des abscisses
-  * ----------------------------------- */
+    // https://github.com/d3/d3-selection/blob/v1.4.0/README.md#selection_attr
+    .attr("viewBox", `0 0 ${ width } ${ height }`)
+  .append("g")
+    .attr("transform", `translate(${ margin.left }, ${ margin.top })`);
 
-const xScale = d3.scaleLinear()
-  .domain( [ 0, 50 ] )
-  .range( [ 0, width - margin.left - margin.right ] );
-
-const xAxis = d3.axisTop().scale(xScale);
-
-svg.append( "g" )
-  .attr( "class", "x axis" )
-  .call( xAxis)
-
-/** -----------------------------------
-  * Création de l'échelle des ordonnées
-  * ----------------------------------- */
-
-const yScale = d3.scaleBand()
-  .padding(0.2);
-
-const yAxis = d3.axisLeft()
-  .scale( yScale )
-  .tickSizeOuter(0)
-
-svg.append( "g" )
-  .attr( "class", "y axis" )
-
-/** -----------------------------------
-  * Fonction qui redessine le graphique à chaque changement de données
-  * -----------------------------------
+// La fonction d3.tsv permet de charger des données depuis un fichier externe de manière asynchrone. Dans le fichier tsv, les données doivent être séparées par des tabulations.
+d3.tsv("data/heures-mmi-s1.tsv").then(function(data) {
+  /**
+    * on ajoute un groupe pour chaque ligne
+    * selectAll() sélectionne tous les groupes "modules" dans le DOM. Comme aucun module n’existe pour l’instant, ça retourne une sélection vide. Voyez cette sélection vide comme représentant les modules qui vont bientôt exister.
   */
+  // debugger;
+  const row = svg.selectAll("g.module")
 
-function update( data ) {
-
-  yScale.range( [ 0, data.length * ( heightModule ) ] );
-
-  if( d3.select( "input" ).property( "checked" ) ) {
-    yScale.domain( data.sort( ( a, b ) => b.heures - a.heures )
-      .map( d => d.module ) )
-  } else {
-    yScale.domain( data.map( d => d.module ) );
-  }
-
-  const textInterpol = function ( d ) {
-    const i = d3.interpolate( this.textContent, d.heures);
-    return t => this.textContent = Math.round( i( t ) );
-  };
-
-  /**
-    * Voyez la constante newRow comme un conteneur des nouvelles données entrantes.
-    * Cela permettra de faire des transitions entre les données existantes, entrantes et sortantes.
+    /**
+      * La méthode data() compte et analyse les valeurs des données.
+      * Il y a 17 modules dans notre ensemble de données, donc à partir de là,
+      * tout ce qui se passe sera exécuté 17 fois, une fois par module.
     */
-  const newRow = svg.selectAll( "g.module" )
-    .data( data );
+    .data(data)
 
-  const row = newRow.enter()
-    .append( "g" )
-    .attr( "class", "module" )
-    .attr( "transform", d => `translate( 0, ${ yScale( d.module ) } )` );
-
-  row.insert( "rect" )
-    .attr( "class", "module_bar" )
-    .attr( "x", 0 )
-    .attr( "opacity", 1 )
-    .attr( "height", yScale.bandwidth() )
-    .attr( "width", 0 )
-      .transition().duration( duration )
-        .attr("width", d => xScale( d.heures ) )
-
-  row.append( "text" )
-    .attr( "class", "module_hours" )
-    .attr( "y", yScale.bandwidth()/2 )
-    .attr( "dy", ".35em" )
-    .attr( "dx", "0.5em" )
-    .attr( "x", 0 )
-    .attr( "opacity", 1 )
-    .text( 0 )
-    .transition().duration( duration )
-      .attr( "x", d => xScale( d.heures ) )
-      .tween( "text", textInterpol );
-
-  svg.select( ".y.axis" )
-    .call( yAxis.tickFormat( d => "M." + d ) )
-    .selectAll( ".tick" )
-      .style( "opacity", 0 )
-      .append( "text" )
-        .attr( "fill", "currentColor" )
-        .attr( "dx", "-7rem" )
-        .attr( "dy", ".35rem" )
-        .data( data )
-          .text( d => d.name )
-
-  /**
-    * Mise à jour du graphique à partir des nouvelles données entrantes
+    /**
+      * Pour créer de nouveaux éléments liés à des données, on doit utiliser la méthode enter().
+      * Cette méthode jette un oeil au DOM, puis aux données qui lui ont été transmises.
+      * S’il y a plus de données que d’éléments DOM correspondants, alors enter() crée un nouvel élément.
+      * Elle transmet ensuite une référence à ce nouveau placeholder au prochain maillon de la chaîne.
     */
+    .enter()
+      .append("g")
+      .attr("class", "module")
 
-  newRow.select( ".module_bar" )
-    .transition().duration( duration )
-      .attr( "width", d => xScale( d.heures ) )
-
-  newRow.select( ".module_hours" )
-    .transition().duration( duration )
-      .attr( "x", d => xScale( d.heures ) )
-      .tween( "text" , textInterpol );
-
-  svg.select( ".y.axis" ).selectAll( ".tick" )
-    .transition().duration( duration / 2 )
-      .style( "opacity", 1 );
-
-  /**
-    * Suppression des données sortantes
-    */
-
-  newRow.exit()
-    .select( ".module_bar" )
-      .transition().duration( duration )
-        .attr( "opacity", 0 )
-        .attr( "width", 0 )
-
-  newRow.exit()
-    .select( ".module_hours" )
-      .transition().duration( duration )
-        .attr( "opacity", 0 )
-        .attr( "x", 0 );
-
-  newRow.exit()
-    .transition().duration( duration )
-      .remove();
-
-  /** -----------------------------------
-    * Tri des modules par heure
-    * -----------------------------------
-    */
-
-  d3.select("input")
-    .on("change", sort);
-
-  function sort() {
-    /*
-    * On redéfinit l'ordre des données
-    */
-    const y = yScale.domain(
-      /*
-      * La fonction sort() retourne une nouvelle sélection contenant une copie de chaque groupe de cette sélection,
-      * triée selon la fonction de comparaison. Après le tri, réinsère les éléments pour qu'ils correspondent à l'ordre obtenu (par sélection.ordre).
-      * La fonction de comparaison, qui est par défaut croissante, reçoit les données a et b de deux éléments à comparer. Il devrait renvoyer une valeur négative, positive ou nulle. Si c'est négatif, alors a devrait être avant b; si positif, alors a devrait être après b; sinon, a et b sont considérés comme égaux et l'ordre est arbitraire.
+      /**
+        * Nos données ont déjà été liées aux éléments "module", donc pour chaque module,
+        *  la valeur de d correspond à la valeur équivalente dans notre ensemble de données à chaque module (1101, 1102, 1103, 1104…).
+        * 'i' est une valeur d’index numérique de l’élément courant.
+        * En partant de 0, donc pour notre “premier” module i == 0, pour le second, i == 1 et ainsi de suite.
+        * On utilise i pour pousser chaque module qui suit un peu plus vers le bas, car à chaque itération la valeur de i est incrémentée (20 étant la hauteur d'un module).
       */
-      // https://github.com/d3/d3-selection/blob/v1.4.0/README.md#selection_sort
-      data.sort(
-        // Opérateur conditionnel
-        // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/L_op%C3%A9rateur_conditionnel
+      .attr("transform", (d, i) => `translate(0, ${ i * (moduleHeight + 2) })`);
 
-        this.checked
-          ? ( a, b ) => b.heures - a.heures
-          : ( a, b) => d3.ascending( a.module, b.module )
+  // Dans chaque ligne, on ajoute un rectangle dont la largeur est difinie en fonction du nombre d'heures du module
+  row.append("rect")
+    .attr("class", "module_bar")
+    .attr("x", 0)
+    .attr("height", moduleHeight)
 
-        ).map ( d => d.module )
-      ).copy();
+    // Ici la fonction nous permet de renourner les heures de chaque module.
+    .attr("width", d => d.heures)
 
-      // console.log(data)
-
-      svg.selectAll( ".module" )
-        .sort( ( a, b ) => y( a.module ) - y( b.module ) );
-
-      svg.select( ".y.axis" ).selectAll( ".tick" )
-        .sort( ( a, b ) => y( a.module ) - y( b.module ) );
-
-    // On définit les transitions pour les nouvelles positions
-
-    const t = svg.transition().duration( duration );
-    // On créé un delay permettant de retarder le début de chaque animation (en milisecondes).
-    const delay = ( d, i ) => i * 50;
-
-
-    t.selectAll( ".module" )
-      .delay( delay )
-        .attr( "transform", d => `translate( 0, ${ y( d.module ) } )` );
-
-    svg.select( ".y.axis" )
-      .transition( t )
-        .call( yAxis )
-        .selectAll( ".tick" )
-          .delay( delay );
-  };
-
-};
-
-/** -----------------------------------
-  * Fonction qui permet de changer de semestre
-  * -----------------------------------
-  */
-function changeSemestre() {
-
-  // On récupère la valeur de l'attibut "value" du select.
-  const semestreSelect = this.value;
-
-  // On créé une fonction semestre avec le paramètre datafile qui associe la valeur du select à une url de fichier
-  const semestre = (datafile) => ({
-    "semestre-1": "data/heures-mmi-s1.tsv",
-    "semestre-2": "data/heures-mmi-s2.tsv",
-    "semestre-3": "data/heures-mmi-s3.tsv",
-    "semestre-4": "data/heures-mmi-s4.tsv"
-  })[datafile]
-
-  // On utilise la fonction d3.tsv
-  d3.tsv( semestre( semestreSelect ) ).then( data => update( data ) );
-};
-
-// On initialise pour le chargement de la page
-d3.tsv("data/heures-mmi-s1.tsv").then( data => update( data ) );
-
-d3.select( "select" ).on( "change", changeSemestre );
+})
